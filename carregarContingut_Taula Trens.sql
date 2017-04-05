48e0c2918c45625a8a47118355cd3603df736364
Joan Esmandia Blanche <u1933574@campus.udg.edu>
2017-04-05 14:59:46 +0200

    Elimino

--- carregarContingut_Taula Trens.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM bdd_apptrens.tren
-LIMIT 0, 1000
-
--- Date: 2017-04-04 20:37
-*/
-INSERT INTO `tren` (`idTren`,`nomTren`,`tipusTren`) VALUES (1,'Regional','R');
-INSERT INTO `tren` (`idTren`,`nomTren`,`tipusTren`) VALUES (2,'Mitja Distancia','MD');
