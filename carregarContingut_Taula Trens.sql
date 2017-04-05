200beca32acb2ca34447d5958f8b639f5ed5b29b
Joan Esmandia Blanche <u1933574@campus.udg.edu>
2017-04-05 14:57:17 +0200

    Afageixo fitxers de la Bade De Dades: Carregar Taules, Carregar Esquema EER, Carregar Contingut Taula Trens, Carregar Contingut Taula Colors.

---
+++ carregarContingut_Taula Trens.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM bdd_apptrens.tren
+LIMIT 0, 1000
+
+-- Date: 2017-04-04 20:37
+*/
+INSERT INTO `tren` (`idTren`,`nomTren`,`tipusTren`) VALUES (1,'Regional','R');
+INSERT INTO `tren` (`idTren`,`nomTren`,`tipusTren`) VALUES (2,'Mitja Distancia','MD');
